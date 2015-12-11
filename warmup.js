8566d6c3a0965ebf859d4f4c3b321f830365d76b
Jon Harlem <jtharlem@gmail.com>
2015-12-11 10:33:26 -0800

    refactored of single page app use

--- warmup.js
+++ warmup.js
@@ -1,3 +1,33 @@
-function test(){
-	
-}
+//always wrap in window.onload
+window.onload = function(){
+
+	$('#myForm').on('submit', function(event) {
+		//prevents default network calls
+		event.preventDefault();
+		console.log("this is working...");
+
+		$.ajax({
+			//want to send it to the browser
+			url: window.location,
+			method: 'POST',
+			data: {
+				//what object am I sending? 1) make ids and extract value 2) 
+				firstName: $('#firstName').val(),
+				lastName: $('#firstName').val(),
+			},
+			//upon success we want to run a function that takes the data from that response
+			success: function(data) {
+				console.log(data);
+				$('#dataHolder').append(data);
+			}
+		});
+	});
+
+};
+
+
+// $("#target").submit(function( event ) {
+// 	if($("input:first").val() === /^[a-z ,.'-]+$/i) {
+// 		$( "" )
+// 	}
+// })
